5a4ac9d7c1ac836651ecd27d47d94f9f3627f2de
aiotter <git@aiotter.com>
2020-05-07 00:47:42 +0900

    Reconnect after mic becomes unavailable

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import sys
+from functools import partial
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,7 +15,18 @@ class MicrophoneClient(discord.Client):
         print(f'Logged in as {self.user}')
         channel = self.get_channel(int(CHANNEL))
         vc = await channel.connect()
-        vc.play(discord.FFmpegPCMAudio(DEVICE, before_options='-f alsa -ac 1'))
+        self.play_mic(vc)
+
+    def play_mic(self, voice_client, error=None):
+        if error:
+            raise error
+        voice_client.play(
+                discord.FFmpegPCMAudio(DEVICE, before_options='-f alsa -ac 1'),
+                after=partial(self.play_mic, voice_client))
+
+    async def on_error(*args, **kwargs):
+        await super().on_error(*args, **kwargs)
+        sys.exit(1)
 
 
 client = MicrophoneClient()
